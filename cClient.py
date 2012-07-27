9d7a2d50934f1dabc6c2613889f65032d8952ea9
Toben Archer <sandslash@gmail.com>
2012-07-27 12:06:27 -0500

    completed the client. it uploads to the db, checks is status, and returns results as a list of (arg,result) tuples. now I just need to update the node to operate with the db. woohoo!

--- cClient.py
+++ cClient.py
@@ -7,20 +7,72 @@
 #		nodes will proceed to bring the data down for processing.
 #################################################
 
+import postgresql
+import random
+
 class Project:
 	def __init__(self,serverIP,projectCode,targetFunction,listOfArgument):
 		self.SIP = serverIP
 		self.code = projectCode
 		self.target = targetFunction
-		self.args = listOfArguments
+		self.args = listOfArgument
+		try:
+			db = postgresql.open("pq://ceranubis:raidersrow@" + self.SIP + "/ceranubis")
+			self.connected = True
+		except:
+			self.connected = False
+			return
+
+		#insert the project into the database
+		self.PID = random.randint(0,2147483647)
+		db.execute("INSERT INTO projects VALUES("+str(self.PID)+",false,'"+self.code+"','"+self.target+"')")
+
+		#insert the arguments into the database:
+		for arg in self.args:
+			AID = random.randint(0,2147483647)
+			db.execute("INSERT INTO arguments VALUES("+str(AID)+","+str(self.PID)+",'"+arg+"')")
 
 	def status(self):
-		pass
+		try:
+			db = postgresql.open("pq://ceranubis:raidersrow@" + self.SIP + "/ceranubis")
+			self.connected = True
+		except:
+			self.connected = False
+			return False
+		
+		getNumDone = db.prepare("SELECT COUNT(result) AS done FROM arguments WHERE result IS NOT NULL  AND arguments.projid = $1;")
+		getNumArg = db.prepare("SELECT COUNT(arg) AS overall FROM arguments WHERE arguments.projid = $1;")
+		done = getNumDone(self.PID)
+		arg = getNumArg(self.PID)
+		return((done[0][0],arg[0][0]))
+		
+
 
 	def results(self):
-		pass
+		try:
+			db = postgresql.open("pq://ceranubis:raidersrow@" + self.SIP + "/ceranubis")
+			self.connected = True
+		except:
+			self.connected = False
+			return False
 
-def commitProject(serverIP,projectCode,targetFunction,listOfArgument):
-	return Project(serverIP,projectCode,targetFunction,listOfArgument)
+		getRes = db.prepare("SELECT arg,result from arguments where result is not null;")	
+		return getRes()
 
-#.S.D.G.#t
+#def commitProject(serverIP,projectCode,targetFunction,listOfArgument):
+#	p = Project(serverIP,projectCode,targetFunction,listOfArgument)
+#	if p.connected:
+#		return p
+#	return False
+
+#q = commitProject('localHost','derpaderpyderp','monkies!',['wooooooo','arg2!'])
+#if q:
+#	print("WOOOOOOOOOOOOOOOOO")
+#input()
+#stat = q.status()
+#print(stat)
+
+#res = q.results()
+#print(res)
+
+#.S.D.G.#
